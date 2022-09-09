69c61eb0264123cf8efc90a71095b565c2d548f7
Greg Lind <greglind@gregs-air.lan>
2022-09-09 07:31:55 -0700

    update config 2

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -93,23 +93,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'health',
-        'PASSWORD': os.environ.get("PASSWORD"),
-        'USER': 'cms_user',
-        'HOST': 'db-mysql-nyc3-97229-do-user-2508039-0.b.db.ondigitalocean.com',
-        'PORT': '25060',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
